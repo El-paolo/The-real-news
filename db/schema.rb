7906a02732b5ff532ae654e4b489fad537a90df9
El-paolo <paolocordovacontacto@gmail.com>
2023-08-13 14:19:20 -0400

    added the navbar and look its well functioning

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_175747) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_180021) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "report_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["report_id"], name: "index_comments_on_report_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "reports", force: :cascade do |t|
     t.string "title"
@@ -35,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_175747) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "reports"
+  add_foreign_key "comments", "users"
 end
