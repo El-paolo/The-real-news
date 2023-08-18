54fff36537bac2ba708f637c8113a168617e2702
El-paolo <paolocordovacontacto@gmail.com>
2023-08-18 16:05:52 -0400

    added the cofigs for the devise forgot password email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,24 @@ Rails.application.configure do
   end
 
   #added for devise configs
+  config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'no-reply@testmail.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: Rails.application.credentials.gmail[:username],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
+#******************************************************************************
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
